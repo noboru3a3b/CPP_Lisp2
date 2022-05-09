6181b5fd22589b372045e0c88e6f929d6bb0941f
Noboru Okada <noboru3a3b@gmail.com>
2022-05-10 07:07:11 +0900

    Update README

--- sample.lisp
+++ sample.lisp
@@ -24,9 +24,9 @@ lt lte mod mul nil nondef not null quote reverse
 sub t
 [exp] Delete Object [Class ID]= 4Cell
 
-> (defun sqr+ (x y) (if (null x) y (sqr+ (cdr x) (+ (* (car x) (car x)) y))))
-[str] (defun sqr+ (x y) (if (null x) y (sqr+ (cdr x) (+ (* (car x) (car x)) y))))
-[exp] (defun sqr+ (x y) (if (null x) y (sqr+ (cdr x) (+ (* (car x) (car x)) y))))
+> (defun sqr+0 (x y) (if (null x) y (sqr+0 (cdr x) (+ (* (car x) (car x)) y))))
+[str] (defun sqr+0 (x y) (if (null x) y (sqr+0 (cdr x) (+ (* (car x) (car x)) y))))
+[exp] (defun sqr+0 (x y) (if (null x) y (sqr+0 (cdr x) (+ (* (car x) (car x)) y))))
 [eval] t
  ---------- atoms ----------
 * + - / < <= = > >= add
@@ -34,12 +34,25 @@ and append atom caaar caadr caar cadar caddar caddr cadr
 car cdaar cdadr cdar cddar cdddr cddr cdr cond cons
 defun div eq eqn gt gte if label lambda list
 lt lte mod mul nil nondef not null quote reverse
-sqr+ sub t x y
+sqr+0 sub t x y
 [exp] Delete Object [Class ID]= 4Cell
 
-> (sqr+ '(1 2 3 4 5) 0)
-[str] (sqr+ (quote (1 2 3 4 5)) 0)
-[exp] (sqr+ (quote (1 2 3 4 5)) 0)
+> (defun sqr+ (x) (sqr+0 x 0))
+[str] (defun sqr+ (x) (sqr+0 x 0))
+[exp] (defun sqr+ (x) (sqr+0 x 0))
+[eval] t
+ ---------- atoms ----------
+* + - / < <= = > >= add
+and append atom caaar caadr caar cadar caddar caddr cadr
+car cdaar cdadr cdar cddar cdddr cddr cdr cond cons
+defun div eq eqn gt gte if label lambda list
+lt lte mod mul nil nondef not null quote reverse
+sqr+ sqr+0 sub t x y
+[exp] Delete Object [Class ID]= 4Cell
+
+> (sqr+ '(1 2 3 4 5))
+[str] (sqr+ (quote (1 2 3 4 5)))
+[exp] (sqr+ (quote (1 2 3 4 5)))
 [eval] 55
  ---------- atoms ----------
 * + - / < <= = > >= add
@@ -47,5 +60,5 @@ and append atom caaar caadr caar cadar caddar caddr cadr
 car cdaar cdadr cdar cddar cdddr cddr cdr cond cons
 defun div eq eqn gt gte if label lambda list
 lt lte mod mul nil nondef not null quote reverse
-sqr+ sub t x y
+sqr+ sqr+0 sub t x y
 [exp] Delete Object [Class ID]= 4Cell
