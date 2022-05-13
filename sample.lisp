2e96f0e5dc33b736103796c68bd770a055d8d7bc
Noboru Okada <noboru3a3b@gmail.com>
2022-05-13 21:03:45 +0900

    Update README

--- sample.lisp
+++ sample.lisp
@@ -14,14 +14,14 @@
 [eval] 10
  ---------- atoms ----------
 * + - / 1+ 1- < <= = >
->= abs acos add and append asin atan atom caaar
-caadr caar cadar caddar caddr cadr car cdaar cdadr cdar
-cddar cdddr cddr cdr cond cons const_e const_pi cos cosh
-dec defun div eq eqn exp floor gt gte if
-inc label lambda let let* list ln log lt lte
-max min mod mul n nil nondef not null pow
-print quote reverse round setq sin sinh sqrt sub t
-tan tanh trunc while
+>= abs acos add and append asin assoc atan atom
+caaar caadr caar cadar caddar caddr cadr car cdaar cdadr
+cdar cddar cdddr cddr cdr cond cons const_e const_pi cos
+cosh dec defun div eq eqn exp floor gt gte
+if inc label lambda let let* list ln log lt
+lte max min mod mul n nil nondef not null
+pow print quote rassoc reverse round setq sin sinh sqrt
+sub t tan tanh trunc while
 [exp] Delete Object [Class ID]= 4Cell
 
 > (let* ((x 10) (y (* 2 x))) (print x) (print y) (cons x y))
@@ -32,14 +32,14 @@ tan tanh trunc while
 [eval] (10 . 20)
  ---------- atoms ----------
 * + - / 1+ 1- < <= = >
->= abs acos add and append asin atan atom caaar
-caadr caar cadar caddar caddr cadr car cdaar cdadr cdar
-cddar cdddr cddr cdr cond cons const_e const_pi cos cosh
-dec defun div eq eqn exp floor gt gte if
-inc label lambda let let* list ln log lt lte
-max min mod mul n nil nondef not null pow
-print quote reverse round setq sin sinh sqrt sub t
-tan tanh trunc while x y
+>= abs acos add and append asin assoc atan atom
+caaar caadr caar cadar caddar caddr cadr car cdaar cdadr
+cdar cddar cdddr cddr cdr cond cons const_e const_pi cos
+cosh dec defun div eq eqn exp floor gt gte
+if inc label lambda let let* list ln log lt
+lte max min mod mul n nil nondef not null
+pow print quote rassoc reverse round setq sin sinh sqrt
+sub t tan tanh trunc while x y
 [exp] Delete Object [Class ID]= 4Cell
 
 > (defun sqr+0 (x y) (if (null x) y (sqr+0 (cdr x) (+ (* (car x) (car x)) y))))
@@ -48,14 +48,14 @@ tan tanh trunc while x y
 [eval] sqr+0
  ---------- atoms ----------
 * + - / 1+ 1- < <= = >
->= abs acos add and append asin atan atom caaar
-caadr caar cadar caddar caddr cadr car cdaar cdadr cdar
-cddar cdddr cddr cdr cond cons const_e const_pi cos cosh
-dec defun div eq eqn exp floor gt gte if
-inc label lambda let let* list ln log lt lte
-max min mod mul n nil nondef not null pow
-print quote reverse round setq sin sinh sqr+0 sqrt sub
-t tan tanh trunc while x y
+>= abs acos add and append asin assoc atan atom
+caaar caadr caar cadar caddar caddr cadr car cdaar cdadr
+cdar cddar cdddr cddr cdr cond cons const_e const_pi cos
+cosh dec defun div eq eqn exp floor gt gte
+if inc label lambda let let* list ln log lt
+lte max min mod mul n nil nondef not null
+pow print quote rassoc reverse round setq sin sinh sqr+0
+sqrt sub t tan tanh trunc while x y
 [exp] Delete Object [Class ID]= 4Cell
 
 > (defun sqr+ (x) (sqr+0 x 0))
@@ -64,14 +64,14 @@ t tan tanh trunc while x y
 [eval] sqr+
  ---------- atoms ----------
 * + - / 1+ 1- < <= = >
->= abs acos add and append asin atan atom caaar
-caadr caar cadar caddar caddr cadr car cdaar cdadr cdar
-cddar cdddr cddr cdr cond cons const_e const_pi cos cosh
-dec defun div eq eqn exp floor gt gte if
-inc label lambda let let* list ln log lt lte
-max min mod mul n nil nondef not null pow
-print quote reverse round setq sin sinh sqr+ sqr+0 sqrt
-sub t tan tanh trunc while x y
+>= abs acos add and append asin assoc atan atom
+caaar caadr caar cadar caddar caddr cadr car cdaar cdadr
+cdar cddar cdddr cddr cdr cond cons const_e const_pi cos
+cosh dec defun div eq eqn exp floor gt gte
+if inc label lambda let let* list ln log lt
+lte max min mod mul n nil nondef not null
+pow print quote rassoc reverse round setq sin sinh sqr+
+sqr+0 sqrt sub t tan tanh trunc while x y
 [exp] Delete Object [Class ID]= 4Cell
 
 > (sqr+ '(1 2 3 4 5))
@@ -80,12 +80,12 @@ sub t tan tanh trunc while x y
 [eval] 55
  ---------- atoms ----------
 * + - / 1+ 1- < <= = >
->= abs acos add and append asin atan atom caaar
-caadr caar cadar caddar caddr cadr car cdaar cdadr cdar
-cddar cdddr cddr cdr cond cons const_e const_pi cos cosh
-dec defun div eq eqn exp floor gt gte if
-inc label lambda let let* list ln log lt lte
-max min mod mul n nil nondef not null pow
-print quote reverse round setq sin sinh sqr+ sqr+0 sqrt
-sub t tan tanh trunc while x y
+>= abs acos add and append asin assoc atan atom
+caaar caadr caar cadar caddar caddr cadr car cdaar cdadr
+cdar cddar cdddr cddr cdr cond cons const_e const_pi cos
+cosh dec defun div eq eqn exp floor gt gte
+if inc label lambda let let* list ln log lt
+lte max min mod mul n nil nondef not null
+pow print quote rassoc reverse round setq sin sinh sqr+
+sqr+0 sqrt sub t tan tanh trunc while x y
 [exp] Delete Object [Class ID]= 4Cell
